63a92412401560cbc0b71b8cb03e3a7a5982eb2f
Taha Baran <22455419@ao.ankara.edu.tr>
2023-05-11 00:32:37 +0300

    Final Update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,18 @@
-ac74d47b1095b70cb42f92b54402bb64
+'use strict';
+
+const api_key = 'ac74d47b1095b70cb42f92b54402bb64';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
--- assets/js/global.js
+++ assets/js/global.js
@@ -18,3 +18,17 @@ const searchTogglers = document.querySelectorAll("[search-toggler]");
 addEventOnElements(searchTogglers, "click", function () {
   searchBox.classList.toggle("active");
 });
+
+/**
+ * store movieId in `localStorage`.
+ * when you click any movie card
+ */
+
+const getMovieDetail = function (movieId) {
+  window.localStorage.setItem("movieId", String(movieId));
+};
+
+const getMovieList = function (urlParam, genreName) {
+  window.localStorage.setItem("urlParam", urlParam);
+  window.localStorage.setItem("genreName", genreName);
+};
